e44b3665ecbcf5c529874fb09b9efa02255413ab
NirajMalokar21 <nirajmalokar@gmail.com>
2024-08-03 14:13:37 +0530

    Change index.js

--- E-Commerce-App/frontend/src/Context/ShopContext.jsx
+++ E-Commerce-App/frontend/src/Context/ShopContext.jsx
@@ -16,12 +16,12 @@ const ShopContextProvider = (props) => {
     const [all_product, setAll_Product] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:4000/allproducts')
+        fetch('https://ecommerce-website-one-dun.vercel.app/allproducts')
         .then((response) => response.json())
         .then((data) => setAll_Product(data))
 
         if(localStorage.getItem('auth-token')){
-            fetch('http://localhost:4000/getcart', {
+            fetch('https://ecommerce-website-one-dun.vercel.app/getcart', {
                 method:'POST',
                 headers:{
                     Accept:'application/form-data',
@@ -39,7 +39,7 @@ const ShopContextProvider = (props) => {
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
         
         if(localStorage.getItem('auth-token')){
-            fetch('http://localhost:4000/addtocart', {
+            fetch('https://ecommerce-website-one-dun.vercel.app/addtocart', {
                 method: 'POST',
                 headers: {
                     Accept:'application/form-data',
@@ -59,7 +59,7 @@ const ShopContextProvider = (props) => {
     const removeFromCart = (itemId) => {
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
         if(localStorage.getItem('auth-token')){
-            fetch('http://localhost:4000/removefromcart', {
+            fetch('https://ecommerce-website-one-dun.vercel.app/removefromcart', {
                 method: 'POST',
                 headers: {
                     Accept:'application/form-data',
