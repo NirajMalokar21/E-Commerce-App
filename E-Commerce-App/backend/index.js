323900096cbe4e4e1163b39e8ceadf2abb691218
NirajMalokar21 <nirajmalokar@gmail.com>
2024-08-03 18:01:45 +0530

    Change backend link

--- E-Commerce-App/backend/index.js
+++ E-Commerce-App/backend/index.js
@@ -33,7 +33,7 @@ app.use('/images', express.static('upload/images'))
 app.post("/upload", upload.single('product'), (req,res)=>{
     res.json({
         success: 1,
-        image_url: `http://localhost:${port}/images/${req.file.filename}`
+        image_url: `https://e-commerce-app-3k8g.onrender.com/images/${req.file.filename}`
     })
 })
 
